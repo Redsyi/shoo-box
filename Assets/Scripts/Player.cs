972159a07c00a033c8a4e8f0310fcddab2e3e484
Nathan Stubbs <stubbsnc@gmail.com>
2020-01-27 21:46:33 -0600

    basic moving box

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public Rigidbody rigidbody;
+    public Animator animator;
     public float speed;
+    public float walkRotation;
 
     private Vector3 currentMovement;
     private float currRotation;
@@ -15,10 +17,12 @@ public class Player : MonoBehaviour
     {
         Vector2 movement = Utilities.RotateVectorDegrees(value.Get<Vector2>().normalized * speed, 135);
         currRotation = -Utilities.VectorToDegrees(movement);
-        transform.eulerAngles = new Vector3(0, currRotation, 0);
+        transform.eulerAngles = new Vector3(walkRotation, currRotation, transform.eulerAngles.z);
 
         currentMovement.x = movement.x;
         currentMovement.z = movement.y;
+
+        animator.SetBool("walking", movement.sqrMagnitude > 0);
     }
 
     private void FixedUpdate()
