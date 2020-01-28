a794eba3e976291466097f3779c82106d426bd7a
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-01-28 17:39:36 -0600

    Added form change, speed change, and collision box change

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,16 +9,28 @@ public class Player : MonoBehaviour
     public Animator animator;
     public float speed;
     public float walkRotation;
+    public GameObject model;
+    public BoxCollider hitBox;
+    public float boxSpeedMultiplier;
 
     private Vector3 currentMovement;
     private float currRotation;
+    private bool legForm;
+    
+
+    private void Start()
+    {
+        //legForm = false;
+    }
 
     public void OnMove(InputValue value)
     {
-        Vector2 movement = Utilities.RotateVectorDegrees(value.Get<Vector2>().normalized * speed, 135);
-        currRotation = -Utilities.VectorToDegrees(movement);
-        transform.eulerAngles = new Vector3(walkRotation, currRotation, transform.eulerAngles.z);
-
+        Vector2 movement = Utilities.RotateVectorDegrees(value.Get<Vector2>().normalized * speed * (legForm ? 1: boxSpeedMultiplier), 135);
+        
+        if(movement.sqrMagnitude != 0)
+        {
+            currRotation = -Utilities.VectorToDegrees(movement);
+        }
         currentMovement.x = movement.x;
         currentMovement.z = movement.y;
 
@@ -27,6 +39,26 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        transform.eulerAngles = new Vector3(walkRotation, currRotation, transform.eulerAngles.z);
         transform.position += currentMovement;
     }
+
+    public void OnChangeForm(InputValue value)
+    {
+        if(!legForm)
+        {
+            if (Physics.Raycast(transform.position, Vector3.up, 1, LayerMask.GetMask("Obstacle")))
+                return;
+        }
+
+        legForm = !legForm;
+        print("Legform = " + legForm);
+
+        model.transform.localPosition = new Vector3(model.transform.localPosition.x, (legForm ? 0: -.65f), model.transform.localPosition.z);
+        hitBox.transform.localPosition = new Vector3(hitBox.transform.localPosition.x, (legForm ? .5f : .2f), hitBox.transform.localPosition.z);
+        hitBox.size = new Vector3(hitBox.size.x, (legForm ? 1 : .3f), hitBox.size.z);
+
+    }
+
+
 }
