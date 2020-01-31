7908dd33d0b062879a135e091078c710ea985f3c
Olivia S <schweers20@utexas.edu>
2020-01-30 18:51:23 -0600

    Revert "Revert "Merge branch 'gamejam' of https://github.com/Redsyi/shoo-box into gamejam""
    This reverts commit 3c8c03db65657f99290487ac277fde8b12a027c2.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     private GameObject player;
-    public Vector3 offset;
+    public enum Direction { NORTH, EAST, SOUTH, WEST};
+    private Direction currDirection;
+    public enum RotationDirection { CLOCKWISE, COUNTERCLOCKWISE};
+    public float cameraSnapRotateSpeed;
+    private float remainingRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,24 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position;
+        if (remainingRotation != 0f)
+        {
+            int direction = (remainingRotation > 0f ? 1 : -1);
+            float rotationAmount = direction * cameraSnapRotateSpeed * Time.deltaTime;
+            if (Mathf.Abs(rotationAmount) > Mathf.Abs(remainingRotation))
+                rotationAmount = remainingRotation;
+            remainingRotation -= rotationAmount;
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + rotationAmount, transform.localEulerAngles.z);
+        }
+    }
+
+    public void Rotate(RotationDirection direction)
+    {
+        int rotationDirection = (direction == RotationDirection.CLOCKWISE ? -1 : 1);
+        if (remainingRotation == 0f)
+        {
+            remainingRotation = 90f * rotationDirection;
+        }
     }
 }
