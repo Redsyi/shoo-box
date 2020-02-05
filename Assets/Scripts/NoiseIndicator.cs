c993afc83536bf60dd3183734a3236ac68515aee
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-02-04 18:37:29 -0600

    Added AI Sound Detection

---
+++ Assets/Scripts/NoiseIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseIndicator : MonoBehaviour
+{
+    public AudioSource source;
+
+    public void MakeNoise(float radius, AudioClip clip, float volume)
+    {
+        if (clip)
+        {
+            source.clip = clip;
+            source.volume = volume;
+            source.Play();
+        }
+
+        if (radius > 0)
+        {
+            foreach (Maid AI in FindObjectsOfType<Maid>())
+            {
+                Vector3 distance = AI.transform.position - transform.position;
+                if (distance.sqrMagnitude <= radius * radius)
+                {
+                    AI.Investigate(gameObject);
+                }
+            }
+        }
+        Invoke("Sudoku", 5);
+    }
+
+    private void Sudoku()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Phone : MonoBehaviour, IKickable
 {
     public GameObject door;
+    public AudioClip clip;
 
     public void OnKick(GameObject kicker)
     {
-        FindObjectOfType<Maid>().Investigate(door);
+        AudioManager.MakeNoise(door.transform.position, 6, clip, 1);
+        //FindObjectOfType<Maid>().Investigate(door);
     }
 }
