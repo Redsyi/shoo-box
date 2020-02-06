03972d9033760597d1194a30ae9bbc6ea0332132
Jake Crabtree <jakercrabtree@gmail.com>
2020-02-05 18:08:56 -0600

    added break hinge ai support

--- Assets/BreakHingeKickable.cs
+++ Assets/BreakHingeKickable.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakHingeKickable : MonoBehaviour, IKickable
+public class BreakHingeKickable : MonoBehaviour, IKickable, IAIInteractable
 {
     // Start is called before the first frame update
     private List<HingeJoint> _hingeJoints;
     [SerializeField] private bool breakAllHinges;
+    private GameObject preBroken = null;
+    private bool broken;
+    [SerializeField] private int fixTime = 4;
     
     void Start()
     {
@@ -25,10 +28,34 @@ public class BreakHingeKickable : MonoBehaviour, IKickable
         }
         else if (_hingeJoints.Count > 0)
         {
+            if (!preBroken && !broken)
+            {
+                preBroken = Instantiate(gameObject);
+                preBroken.SetActive(false);
+            }
             int randIndex = Random.Range(0, _hingeJoints.Count);
             HingeJoint hingeJoint = _hingeJoints[randIndex];
             _hingeJoints.RemoveAt(randIndex);
             Destroy(hingeJoint);
         }
+        broken = true;
+    }
+    
+    public bool NeedsInteraction()
+    {
+        return broken;
+    }
+    
+    public void AIInteract()
+    {
+        broken = false;
+        preBroken.SetActive(true);
+        preBroken = null;
+        Destroy(gameObject);
+    }
+
+    public float AIInteractTime()
+    {
+        return fixTime;
     }
 }
