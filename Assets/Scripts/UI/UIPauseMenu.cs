a246c06388380c11c040fc3216a4354b7264dcff
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-11 18:26:23 -0600

    ported pause menu and shoe tag

---
+++ Assets/Scripts/UI/UIPauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPauseMenu : MonoBehaviour
+{
+    public bool paused { get; private set; }
+    public static UIPauseMenu instance;
+
+    private void Start()
+    {
+        instance = this;
+        gameObject.SetActive(false);
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
+        gameObject.SetActive(paused);
+        Time.timeScale = (paused ? 0 : 1);
+    }
+
+    public void OnRetryPressed()
+    {
+        Debug.Log("Retry");
+    }
+
+    public void OnExitPressed()
+    {
+        Debug.Log("Exit");
+    }
+}
---
+++ Assets/Scripts/UI/UIShoeTagState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShoeTagState : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        if (animatorStateInfo.IsName("SwitchTags"))
+        {
+            FindObjectOfType<UIShoeTag>().FinishedSwitchTags();
+        }
+    }
+}
