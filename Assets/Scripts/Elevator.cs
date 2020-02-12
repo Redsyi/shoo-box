91100008ac9de089e6e8080dcf15e77d1dbc1eff
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-02-12 16:50:49 -0600

    Add elevator scene change

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Elevator : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        print(other.transform.parent.gameObject.name);
+
+        if (other.transform.parent.gameObject.name == "Player")
+        {
+            //Transform leftDoor = other.transform.parent.GetChild(2);
+            //Transform rightDoor = other.transform.parent.GetChild(3);
+            //leftDoor.position = new Vector3(leftDoor.position.x, leftDoor.position.y, -2.61f);
+            //rightDoor.position = new Vector3(rightDoor.position.x, rightDoor.position.y, 2.29f);
+            SceneManager.LoadScene(1);
+        }
+    }
+}
