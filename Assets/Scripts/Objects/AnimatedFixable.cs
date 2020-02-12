2a8ad612c077730847908b7af68af55fc1430788
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-12 13:31:49 -0600

    added chair prefab

---
+++ Assets/Scripts/Objects/AnimatedFixable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedFixable : MonoBehaviour, IAIInteractable
+{
+    public Animator animator;
+    public string breakTriggerName = "Break";
+    public string fixTriggerName = "Fix";
+    public float fixTime = 3;
+    public AIInterest[] interestLayers;
+    private bool broken;
+
+    public void AIFinishInteract()
+    {
+        broken = false;
+        animator.SetTrigger(fixTriggerName);
+    }
+
+    public void AIInteracting(float interactProgress)
+    {
+    }
+
+    public float AIInteractTime()
+    {
+        return fixTime;
+    }
+
+    public AIInterest[] InterestingToWhatAI()
+    {
+        return interestLayers;
+    }
+
+    public bool NeedsInteraction()
+    {
+        return broken;
+    }
+
+    public void Break()
+    {
+        broken = true;
+        animator.SetTrigger(breakTriggerName);
+    }
+}
