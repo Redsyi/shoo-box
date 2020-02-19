918d3ddb27bbc47c7fedad2da40f4ab52fac0225
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-19 17:26:36 -0600

    started reintegrating sfx and musi

---
+++ Assets/Scripts/Objects/MakeSoundOnKick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeSoundOnKick : MonoBehaviour, IKickable
+{
+    public AudioClip clip;
+    public float AIRadius;
+    public float volume = 1;
+
+    public void OnKick(GameObject kicker)
+    {
+        AudioManager.MakeNoise(transform.position, AIRadius, clip, volume);
+    }
+}
