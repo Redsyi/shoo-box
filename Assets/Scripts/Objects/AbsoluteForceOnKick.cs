52d3b5e713e86a50e0c6d3d228ffc65e5bb5e2b4
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-24 19:04:19 -0600

    tv jankiness fixed

---
+++ Assets/Scripts/Objects/AbsoluteForceOnKick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsoluteForceOnKick : MonoBehaviour, IKickable
+{
+    public Vector3 force;
+    private Rigidbody rigidbody;
+
+    public void OnKick(GameObject kicker)
+    {
+        rigidbody.AddRelativeForce(force);
+    }
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+}
