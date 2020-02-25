ced31c8f20a65242933b78f89f244ce8e6c59d86
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-24 18:54:04 -0600

    ai now catches player based on hitbox, not distance

---
+++ Assets/Scripts/AI/AIPlayerCatcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPlayerCatcher : MonoBehaviour
+{
+    public AIAgent agent;
+    private void OnTriggerEnter(Collider other)
+    {
+        print("collider enter");
+        Player player = other.GetComponentInParent<Player>();
+        if (agent.currState.state == AIState.CHASE && player != null)
+        {
+            print("PLAYER");
+            agent.CatchPlayer(player);
+        }
+    }
+}
