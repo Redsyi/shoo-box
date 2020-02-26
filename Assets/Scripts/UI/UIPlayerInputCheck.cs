b04e464470567fdec908e662f4c37c095e905359
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-02-26 17:18:08 -0600

    tutorial popups

---
+++ Assets/Scripts/UI/UIPlayerInputCheck.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPlayerInputCheck : MonoBehaviour
+{
+
+    private UITutorialManager manager;
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<UITutorialManager>();
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manager.controller = player.usingController;
+        if (player.shoeManager.currShoe == ShoeType.BOOTS)
+            manager.ShowUse();
+    }
+
+    public void OnMove()
+    {
+        manager.ShowCamera();
+    }
+
+    public void OnRotate()
+    {
+        manager.ShowLegform();
+    }
+
+    public void OnChangeForm()
+    {
+        manager.ShowInteract();
+    }
+
+    public void OnAction()
+    {
+        manager.FinishUse();
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/UI/UITutorialManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITutorialManager : MonoBehaviour
+{
+
+    private bool movement;
+    private bool camera;
+    private bool legform;
+    private bool interact;
+    private bool use;
+    private Animator currAnimator;
+    private ShoePickup boots;
+    
+    public Animator movementAni;
+    public Animator cameraAni;
+    public Animator legformAni;
+    public Animator interactAni;
+    public Animator useAni;
+    public bool controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ShowMovement", 2);
+        boots = FindObjectOfType<ShoePickup>();
+    }
+
+    void Update()
+    {
+        if (currAnimator)
+        {
+            currAnimator.SetBool("Controller", controller);
+        }
+
+        if (interactAni)
+        {
+            interactAni.transform.position = CameraScript.current.camera.WorldToScreenPoint(boots.transform.position) + Vector3.down * 150;
+        }
+    }
+
+    void ShowMovement()
+    {
+
+        if(!movement)
+        {
+            movement = true;
+            movementAni.gameObject.SetActive(true);
+            currAnimator = movementAni;
+        }
+    }
+
+    public void ShowCamera()
+    {
+        if (movement && !camera)
+        {
+            movementAni.gameObject.SetActive(false);
+            camera = true;
+            cameraAni.gameObject.SetActive(true);
+            currAnimator = cameraAni;
+        }
+    }
+
+    public void ShowLegform()
+    {
+        if (camera && !legform)
+        {
+            cameraAni.gameObject.SetActive(false);
+            legform = true;
+            legformAni.gameObject.SetActive(true);
+            currAnimator = legformAni;
+        }
+    }
+
+    public void ShowInteract()
+    {
+        if (legform && !interact)
+        {
+            legformAni.gameObject.SetActive(false);
+            interact = true;
+            interactAni.gameObject.SetActive(true);
+            currAnimator = interactAni;
+        }
+    }
+
+    public void ShowUse()
+    {
+        if (interact && !use)
+        {
+            interactAni.gameObject.SetActive(false);
+            use = true;
+            useAni.gameObject.SetActive(true);
+            currAnimator = useAni;
+        }
+    }
+
+    public void FinishUse()
+    {
+        if (use)
+        {
+            useAni.gameObject.SetActive(false);
+            currAnimator = null;
+        }
+    }
+}
