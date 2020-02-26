795674af410677e19c22ba027e5de72baad27053
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-26 14:31:40 -0600

    added out of bounds indicator for invisible wall in lobby

---
+++ Assets/Scripts/Testing/OutOfBounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private MeshRenderer renderer;
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        renderer.material.SetFloat("_Alpha", 0.8f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        renderer.material.SetFloat("_Alpha", 0f);
+    }
+}
