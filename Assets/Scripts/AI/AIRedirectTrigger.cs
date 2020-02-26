27e4a770871dc43ba668017256bf4bbcf3636eed
Nathan Stubbs <stubbsnc@gmail.com>
2020-02-26 14:47:22 -0600

    Maid will now check on trashcan, lamp, and toilet in hotel room wen they're broken; rebaked navmesh on top floor

---
+++ Assets/Scripts/AI/AIRedirectTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIRedirectTrigger : MonoBehaviour
+{
+    public GameObject[] fixablesToCheck;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AIAgent ai = other.GetComponentInParent<AIAgent>();
+        if (ai)
+        {
+            foreach (GameObject fixable in fixablesToCheck)
+            {
+                if (fixable.GetComponent<IAIInteractable>().NeedsInteraction())
+                {
+                    ai.Investigate(fixable.gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
