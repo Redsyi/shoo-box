886ca883f30c40e8b9a6ba8b250d8e8faf06d0ab
Delong-Miki-Lin <braverlin@outlook.com>
2020-03-04 16:59:21 -0600

    Wwise Integration for Windows/Linux

---
+++ Assets/Wwise/Deployment/API/Handwritten/Linux/AkLinuxSettings.cs
@@ -0,0 +1,10 @@
+﻿public class AkLinuxSettings : AkWwiseInitializationSettings.CommonPlatformSettings
+{
+#if UNITY_EDITOR
+	[UnityEditor.InitializeOnLoadMethod]
+	private static void AutomaticPlatformRegistration()
+	{
+		RegisterPlatformSettingsClass<AkLinuxSettings>("Linux");
+	}
+#endif // UNITY_EDITOR
+}
