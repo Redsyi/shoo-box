0655b43fb65220c7ab174433fa5491f0b8b0ec29
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-03-03 19:42:18 -0600

    Added conveyor belt mechanics

---
+++ Assets/Scripts/Objects/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public Vector3 move;
+    public float speed;
+    public static bool active = true;
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(active)
+        {
+            Player player = other.GetComponentInParent<Player>();
+
+            if(player)
+                player.transform.position += move * speed * Time.fixedDeltaTime;
+        }
+    }
+
+    public static void ToggleActive()
+    {
+        active = !active;
+
+        foreach (ConveyorBelt conveyorBelt in FindObjectsOfType<ConveyorBelt>())
+            conveyorBelt.GetComponent<MeshRenderer>().material.SetFloat("_MoveSpeed", (active ? 1 : 0));
+    }
+    
+}
---
+++ Assets/Scripts/Objects/ConveyorBeltSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltSwitch : MonoBehaviour, IKickable, ISandalable
+{
+
+
+    public void HitBySandal()
+    {
+        ConveyorBelt.ToggleActive();
+    }
+
+    public void OnKick(GameObject kicker)
+    {
+        ConveyorBelt.ToggleActive();
+    }
+}
