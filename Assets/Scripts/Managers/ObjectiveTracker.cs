47561431c1e9103a44afee91f57a08c477057d0b
Nathan Stubbs <stubbsnc@gmail.com>
2020-03-04 16:29:52 -0600

    hooked up all levels, added storyboard

---
+++ Assets/Scripts/Managers/ObjectiveTracker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveTracker : MonoBehaviour
+{
+    public string[] objectives;
+    private int currObjective;
+    private int currAnimatedObjective;
+    public static ObjectiveTracker instance;
+    private bool animating;
+    public Animator animator;
+    public TextMesh text;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (!animating && currAnimatedObjective < currObjective)
+        {
+            ++currAnimatedObjective;
+            animating = true;
+        }
+    }
+
+    IEnumerator AnimateObjective()
+    {
+        animating = true;
+        animator.SetTrigger("Complete");
+        yield return null;
+    }
+
+    public void CompleteObjective(int objectiveNum)
+    {
+        if (objectiveNum > currObjective)
+            currObjective = objectiveNum;
+    }
+}
---
+++ Assets/Scripts/Utility/ChangeSceneAfterSeconds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneAfterSeconds : MonoBehaviour
+{
+    public int destinationScene;
+    public float afterSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangeScene", afterSeconds);
+    }
+
+    void ChangeScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(destinationScene);
+    }
+}
