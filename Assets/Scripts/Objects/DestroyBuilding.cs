e12226b2e46ac95bf25939d6b5ed02c9c3095316
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-03-09 18:21:41 -0500

    Added dust effect to buidling

--- Assets/Scripts/Objects/DestroyBuilding.cs
+++ Assets/Scripts/Objects/DestroyBuilding.cs
@@ -6,24 +6,44 @@ public class DestroyBuilding : MonoBehaviour, IKickable
 {
     public int maxKicks;
     public float toGround;
+    public ParticleSystem dust;
+    public float speed;
 
     private float originalY;
     private float newY;
     private int numKicks = 0;
+    private int numDestroys = 0;
    
     public void OnKick(GameObject kicker)
     {
         numKicks++;
         if (numKicks < maxKicks)
         {
+         
             newY -= (originalY / maxKicks);
-            print("Original: " + originalY);
-            print("New: " + newY);
-            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            StartCoroutine(Destroy());
         }
         else if (numKicks == maxKicks)
-            transform.position = new Vector3(transform.position.x, toGround, transform.position.z);
-        
+        {
+            newY = toGround;
+            StartCoroutine(Destroy());
+        }
+    }
+
+    private IEnumerator Destroy()
+    {
+        numDestroys++;
+        CameraScript.current.ShakeScreen(ShakeStrength.INTENSE, ShakeLength.MEDIUM);
+        dust.gameObject.SetActive(true);
+        while(transform.position.y > newY)
+        {
+            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1.5f);
+        numDestroys--;
+        if(numDestroys == 0)
+            dust.gameObject.SetActive(false);
 
     }
 
