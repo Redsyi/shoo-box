4fd22c44049e5152374cb4d7d8af606ea9c26030
Nathan Stubbs <stubbsnc@gmail.com>
2020-03-11 12:51:21 -0500

    decorated and added outlines and objectives to conveyor scene

--- Assets/Scripts/Objects/ConveyorBeltSwitch.cs
+++ Assets/Scripts/Objects/ConveyorBeltSwitch.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class ConveyorBeltSwitch : MonoBehaviour, IKickable, ISandalable
 {
+    private Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void HitBySandal()
     {
         ConveyorBelt.ToggleActive();
+        animator.SetTrigger("Trigger");
     }
 
     public void OnKick(GameObject kicker)
     {
         ConveyorBelt.ToggleActive();
+        animator.SetTrigger("Trigger");
     }
 }
