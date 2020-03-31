24d8bf4973f6b36ba89b632f4db4f88395d69c22
unknown <nathan.stubbs@utexas.edu>
2020-03-31 17:46:50 -0500

    new and improved sandals

--- Assets/Scripts/Player/Shoes/SandalProjectile.cs
+++ Assets/Scripts/Player/Shoes/SandalProjectile.cs
@@ -8,19 +8,29 @@ public class SandalProjectile : MonoBehaviour
     private bool reversing;
     public Collider hitbox;
     public float speed = 1;
-    public float maxDist = 30f;
+    [HideInInspector]
+    public float maxDist;
     private Vector3 originalPos;
     [HideInInspector]
     public SandalSlinger slinger;
+    public Transform target;
+    public Transform model;
+    public float spinSpeed;
 
     private void Start()
     {
         originalPos = transform.position;
+        if (target != null)
+        {
+            transform.LookAt(target);
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-            reversing = true;
-            hitbox.enabled = false;
+        reversing = true;
+        hitbox.enabled = false;
+        CameraScript.current.ShakeScreen(ShakeStrength.MEDIUM, ShakeLength.SHORT);
+        Player.ControllerRumble(RumbleStrength.MEDIUM, 0.2f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -52,8 +62,14 @@ public class SandalProjectile : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
         if (reversing && (transform.position - slinger.transform.position).sqrMagnitude <= 0.5f)
         {
+            slinger.rightSandalModel.enabled = true;
             slinger.slinging = false;
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        model.localEulerAngles = new Vector3(0, model.localEulerAngles.y + Time.deltaTime * spinSpeed, 0);
     }
 }
--- Assets/Scripts/Player/Shoes/SandalSlinger.cs
+++ Assets/Scripts/Player/Shoes/SandalSlinger.cs
@@ -6,6 +6,41 @@ public class SandalSlinger : MonoBehaviour
 {
     public SandalProjectile sandalPrefab;
     public bool slinging;
+    public LineRenderer shotPreview;
+    private bool _showShotPreview;
+    public bool holdingShot
+    {
+        get
+        {
+            return _showShotPreview;
+        }
+        set
+        {
+            if (_showShotPreview != value)
+            {
+                _showShotPreview = value;
+                shotPreview.enabled = value;
+                targetCollider.enabled = value;
+                if (!value)
+                {
+                    targets.Clear();
+                    currTarget = null;
+                }
+            }
+        }
+    }
+    public float maxDist;
+    public Transform currTarget;
+    public Vector2 desiredForward;
+    private HashSet<Transform> targets;
+    public SphereCollider targetCollider;
+    public Vector3 vectorToTarget => (currTarget ? (currTarget.position - transform.position) : Vector3.zero);
+    public SkinnedMeshRenderer rightSandalModel;
+
+    private void Start()
+    {
+        targets = new HashSet<Transform>();
+    }
 
     public void Sling()
     {
@@ -13,7 +48,81 @@ public class SandalSlinger : MonoBehaviour
         {
             SandalProjectile sandal = Instantiate(sandalPrefab, transform.position, transform.rotation);
             sandal.slinger = this;
+            sandal.maxDist = maxDist;
+            sandal.target = currTarget;
+            rightSandalModel.enabled = false;
             slinging = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (holdingShot)
+        {
+            CalculateBestTarget();
+            if (currTarget)
+            {
+                shotPreview.SetPosition(1, currTarget.position);
+            } else
+            {
+                RaycastHit hitResult;
+                if (Physics.Raycast(transform.position, transform.forward, out hitResult, maxDist, LayerMask.GetMask("Default", "Obstructions")))
+                {
+                    shotPreview.SetPosition(1, hitResult.point);
+                } else
+                {
+                    shotPreview.SetPosition(1, transform.position + transform.forward*maxDist);
+                }
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (holdingShot)
+        {
+            shotPreview.SetPosition(0, shotPreview.transform.position);
+        }
+    }
+
+    void CalculateBestTarget()
+    {
+        if (targets.Count == 0)
+        {
+            currTarget = null;
+            return;
+        }
+        float minAngleDiff = 99999;
+        foreach (Transform target in targets)
+        {
+            Vector3 vectToTarget = (target.position - transform.position);
+            Vector2 normalizedVectToTarget = new Vector2(vectToTarget.x, vectToTarget.z).normalized;
+            float angleDiff = Mathf.Acos(Vector2.Dot(desiredForward, normalizedVectToTarget));
+            if (angleDiff < minAngleDiff)
+            {
+                currTarget = target;
+                minAngleDiff = angleDiff;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!targets.Contains(other.transform))
+            targets.Add(other.transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (targets.Contains(other.transform))
+            targets.Remove(other.transform);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (targetCollider)
+        {
+            targetCollider.radius = maxDist;
         }
     }
 }
