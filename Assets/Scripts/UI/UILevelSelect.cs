06277bd25f4aeadb5937b842bc02b055db116d73
Olivia S <schweers20@utexas.edu>
2020-04-08 10:16:10 -0500

    Level Select first pass in test scene
    Messy code & messy hierarchy but it's there

---
+++ Assets/Scripts/UI/UILevelSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILevelSelect : MonoBehaviour
+{
+    public int levelIndex;
+    public string levelName;
+
+    public void LevelSelect()
+    {
+        LevelBridge.BridgeTo(levelIndex, levelName);
+    }
+}
---
+++ Assets/Scripts/UI/UIOptionsTabGroup.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIOptionsTabGroup : MonoBehaviour
+{
+    public List<UIOptionsTabButton> tabButtons;
+    public List<GameObject> objectsToSwap;
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+    public UIOptionsTabButton selectedTab;
+
+    public void Subscribe(UIOptionsTabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<UIOptionsTabButton>();
+        }
+
+        tabButtons.Add(button);
+
+        //Make first tab sprite automatically selected
+        if (selectedTab == null && tabButtons.Count > 2)
+        {
+            selectedTab = tabButtons[2];
+            tabButtons[2].background.sprite = tabActive;
+            tabButtons[2].background.color = new Color(0.9f, 0.9f, 0.9f);
+        }
+    }
+
+    public void OnTabEnter(UIOptionsTabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+            button.background.color = new Color(0.9f,0.9f,0.9f);
+        }
+        
+    }
+
+    public void OnTabExit(UIOptionsTabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(UIOptionsTabButton button)
+    {
+        if (selectedTab != null)
+        {
+            selectedTab.Deselect();
+        }
+
+        selectedTab = button;
+
+        selectedTab.Select();
+
+        ResetTabs();
+        button.background.sprite = tabActive;
+
+        //Swap tab pages on select
+        //index incorrect bc using PauseMenu prefab; change later
+        int index = button.transform.GetSiblingIndex() - 7;
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach (UIOptionsTabButton button in tabButtons)
+        {
+            if (selectedTab != null && button == selectedTab)
+            {
+                continue;
+            }
+            button.background.sprite = tabIdle;
+            button.background.color = Color.white;
+        }
+    }
+}
