5c7b6916a361b08e4032de26f371e2eea1bcaa95
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-04-08 12:27:48 -0500

    Added City terminal

---
+++ Assets/Scripts/Objects/AnimateOnEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateOnEnter : MonoBehaviour
+{
+    public Animator animator;
+    public string triggerName;
+
+    public void OnEnter(GameObject kicker)
+    {
+        animator.SetTrigger(triggerName);
+    }
+
+    private void Start()
+    {
+        if (animator == null)
+            Debug.LogError("AnimateOnEnter not attached to animator");
+    }
+}
