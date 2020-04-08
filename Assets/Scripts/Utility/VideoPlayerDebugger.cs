242f16a0612fb45f04887e5af9bd6949a12efe64
unknown <nathan.stubbs@utexas.edu>
2020-04-08 15:06:53 -0500

    fixed intro cutscene not playing on windows

---
+++ Assets/Scripts/Utility/VideoPlayerDebugger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerDebugger : MonoBehaviour
+{
+    private VideoPlayer player;
+    private void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+        
+    }
+
+    private void Update()
+    {
+        print(player.isPlaying);
+    }
+}
