33bc3ecbf3a54049130aa903d6710634d606b3ae
unknown <nathan.stubbs@utexas.edu>
2020-04-16 14:36:54 -0500

    integrated Y button in swap tutorial

--- Assets/Scripts/Objects/ConveyorBeltSwitch.cs
+++ Assets/Scripts/Objects/ConveyorBeltSwitch.cs
@@ -24,52 +24,12 @@ public class ConveyorBeltSwitch : MonoBehaviour, IKickable, ISandalable, IAIInte
             if (fling && fling.AIs.Length > 0)
                 fling.OnFling();
         }
-        /*if (ConveyorBelt.active)
-        {
-            ChangeLevel changeLevel = FindObjectOfType<ChangeLevel>();
-            if (changeLevel)
-            {
-                print("enabling changeLevel");
-                changeLevel.canChangeLevels = true;
-            }
-        }
-        else
-        {
-            SummonOnFling fling = GetComponent<SummonOnFling>();
-            if (fling && fling.AIs.Length > 0)
-                fling.OnFling();
-            ChangeLevel changeLevel = FindObjectOfType<ChangeLevel>();
-            if (changeLevel)
-            {
-                print("Unenabling changeLevel");
-                if(!changeLevel.lockChange)
-                    changeLevel.canChangeLevels = false;
-            }
-        }*/
     }
 
     public void OnKick(GameObject kicker)
     {
         ConveyorBelt.ToggleActive();
         animator.SetTrigger("Trigger");
-        /*if (ConveyorBelt.active)
-        {
-            ChangeLevel changeLevel = FindObjectOfType<ChangeLevel>();
-            if (changeLevel)
-            {
-                print("enabling changeLevel");
-                changeLevel.canChangeLevels = true;
-            }
-        }
-        else
-        {
-            ChangeLevel changeLevel = FindObjectOfType<ChangeLevel>();
-            if (changeLevel)
-            {
-                print("Unenabling changeLevel");
-                changeLevel.canChangeLevels = false;
-            }
-        }*/
     }
 
     public float AIInteractTime()
@@ -81,24 +41,6 @@ public class ConveyorBeltSwitch : MonoBehaviour, IKickable, ISandalable, IAIInte
     {
         ConveyorBelt.ToggleActive();
         animator.SetTrigger("Trigger");
-       /* if (ConveyorBelt.active)
-        {
-            ChangeLevel changeLevel = FindObjectOfType<ChangeLevel>();
-            if (changeLevel)
-            {
-                print("enabling changeLevel");
-                changeLevel.canChangeLevels = true;
-            }
-        }
-        else
-        {
-            ChangeLevel changeLevel = FindObjectOfType<ChangeLevel>();
-            if (changeLevel)
-            {
-                print("Unenabling changeLevel");
-                changeLevel.canChangeLevels = false;
-            }
-        }*/
     }
 
     public void AIInteracting(float interactProgress)
