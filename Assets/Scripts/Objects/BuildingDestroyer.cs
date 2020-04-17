26c6b6851deb070c14e995fd90d444ff480915b5
unknown <nathan.stubbs@utexas.edu>
2020-04-17 18:14:43 -0500

    kickable fountains

---
+++ Assets/Scripts/Objects/BuildingDestroyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BuildingDestroyer : MonoBehaviour
+{
+    public float minVelocity;
+    float minVelocitySqr => minVelocity * minVelocity;
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyBuilding building = collision.gameObject.GetComponent<DestroyBuilding>();
+        if (building)
+        {
+            if (rigidbody.velocity.sqrMagnitude >= minVelocitySqr)
+            {
+                building.OnKick(gameObject);
+                AkEventOnKick soundComponent = building.GetComponent<AkEventOnKick>();
+                if (soundComponent)
+                {
+                    building.GetComponent<AkEventOnKick>().OnKick(gameObject);
+                } else
+                {
+                    Debug.LogWarning($"{building.gameObject.name} has no AkEventOnKick");
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Objects/ForceOnKick.cs
+++ Assets/Scripts/Objects/ForceOnKick.cs
@@ -9,11 +9,20 @@ public class ForceOnKick : MonoBehaviour, IKickable
     private Rigidbody rigidbody;
     public bool kickEnabled = true;
     public Vector3 additionalForceVector;
+    public bool addRandomTorque;
 
     public void OnKick(GameObject kicker)
     {
         if (kickEnabled)
+        {
+            if (rigidbody.isKinematic)
+                rigidbody.isKinematic = false;
             rigidbody.AddForce(((transform.position - kicker.transform.position).normalized + additionalForceVector) * force);
+            if (addRandomTorque)
+            {
+                rigidbody.AddTorque(new Vector3(Random.Range(-20f, 20f), Random.Range(-20f, 20f), Random.Range(-20f, 20f)));
+            }
+        }
     }
 
     // Start is called before the first frame update
