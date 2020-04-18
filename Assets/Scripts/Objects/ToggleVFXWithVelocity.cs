98cf2d490366c433bf420e5e5d6c487a87cd0967
unknown <nathan.stubbs@utexas.edu>
2020-04-17 22:23:38 -0500

    added traffic light prefabs

---
+++ Assets/Scripts/Objects/ToggleVFXWithVelocity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class ToggleVFXWithVelocity : MonoBehaviour
+{
+    public float threshold;
+    float thresholdSqr => threshold * threshold;
+
+    Rigidbody rigidbody;
+    VisualEffect visualEffect;
+    bool active;
+
+    void Start()
+    {
+        rigidbody = GetComponentInParent<Rigidbody>();
+        visualEffect = GetComponent<VisualEffect>();
+    }
+
+    private void FixedUpdate()
+    {
+        bool shouldBeActive = rigidbody.velocity.sqrMagnitude >= thresholdSqr;
+        if (shouldBeActive && !active)
+        {
+            active = true;
+            visualEffect.Play();
+        } else if (!shouldBeActive && active)
+        {
+            active = false;
+            visualEffect.Stop();
+        }
+    }
+}
