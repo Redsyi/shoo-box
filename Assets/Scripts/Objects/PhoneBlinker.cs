401f629e1636ce31e0f38362fee57de3a61ac873
unknown <nathan.stubbs@utexas.edu>
2020-04-19 16:15:54 -0500

    blinky phone

---
+++ Assets/Scripts/Objects/PhoneBlinker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PhoneBlinker : MonoBehaviour, IKickable
+{
+    public float timeToBlink;
+
+    VisualEffect blinker;
+    float timeSinceLastKick;
+    Player player;
+
+    void Start()
+    {
+        blinker = GetComponentInChildren<VisualEffect>();
+        player = FindObjectOfType<Player>();
+    }
+
+    public void OnKick(GameObject kicker)
+    {
+        timeSinceLastKick = 0f;
+        blinker.Stop();
+    }
+    
+    void Update()
+    {
+        if (player.shoeManager.currShoe != ShoeType.BAREFOOT)
+        {
+            if (timeSinceLastKick < timeToBlink)
+            {
+                timeSinceLastKick += Time.deltaTime;
+                if (timeSinceLastKick >= timeToBlink)
+                {
+                    blinker.Play();
+                }
+            }
+        }
+    }
+}
