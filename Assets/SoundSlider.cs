ef13beeffa32ff243ec7a24f37ccaeb375cd6660
Jake Crabtree <uxsee3@gmail.com>
2020-04-21 14:00:20 -0700

    soundslider

--- Assets/SoundSlider.cs
+++ Assets/SoundSlider.cs
@@ -7,34 +7,8 @@ public class SoundSlider : MonoBehaviour
 {
     [SerializeField] string rtpcName;
 
-    public Slider thisSlider;
-    public float masterVolume;
-    public float musicVolume;
-    public float SFXVolume;
-
-
-    public void SetSpecificVolume(string whatValue)
+    public void OnValueChanged(float volume)
     {
-        float sliderValue = thisSlider.value;
-        
-        if (whatValue == "Master")
-        {
-            masterVolume = thisSlider.value;
-            AkSoundEngine.SetRTPCValue("MatserVolume", masterVolume);
-        }
-
-        if (whatValue == "Music")
-        {
-            masterVolume = thisSlider.value;
-            AkSoundEngine.SetRTPCValue("MusicVolume", masterVolume);
-        }
-
-        if (whatValue == "Master")
-        {
-            masterVolume = thisSlider.value;
-            AkSoundEngine.SetRTPCValue("MatserVolume", masterVolume);
-        }
-
-        //AkSoundEngine.SetRTPCValue(rtpcName, volume);
+        AkSoundEngine.SetRTPCValue(rtpcName, volume);
     }
 }
