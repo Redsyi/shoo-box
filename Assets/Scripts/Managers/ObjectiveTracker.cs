144214c179268f680511ff468f90267ad42a00dd
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-04-21 17:45:35 -0500

    Updated objective tracker and objectives for terminal, city terminal and city

--- Assets/Scripts/Managers/ObjectiveTracker.cs
+++ Assets/Scripts/Managers/ObjectiveTracker.cs
@@ -1,31 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveTracker : MonoBehaviour
 {
     public string[] objectives;
-    private int currObjective;
+    private int currObjective = 0;
     public static ObjectiveTracker instance;
     private List<UIObjective> objectiveUIs;
     public UIObjective objectivePrefab;
+    public Text objectiveTagText;
 
     private void Awake()
     {
         instance = this;
-        objectiveUIs = new List<UIObjective>();
-        foreach (string objectiveText in objectives)
+        /* objectiveUIs = new List<UIObjective>();
+         foreach (string objectiveText in objectives)
+         {
+             UIObjective newObjective = Instantiate(objectivePrefab, transform);
+             newObjective.SetText(objectiveText);
+             objectiveUIs.Add(newObjective);
+         }*/
+        if (objectiveTagText && objectives.Length > 0)
         {
-            UIObjective newObjective = Instantiate(objectivePrefab, transform);
-            newObjective.SetText(objectiveText);
-            objectiveUIs.Add(newObjective);
+            objectiveTagText.text = objectives[currObjective];
         }
+       
     }
 
     public void CompleteObjective(int objectiveNum)
     {
+        print("Current objective number: " + currObjective);
         UIShoeSightReminder.instance.ObjectiveCompleted();
-        objectiveUIs[objectiveNum].Complete();
-        currObjective = objectiveNum;
+        //objectiveUIs[objectiveNum].Complete();
+        currObjective++;
+        print("Next objeective number: " + currObjective);
+        if (currObjective < objectives.Length)
+            objectiveTagText.text = objectives[currObjective]; // Update the objective tag with the next 
+        else
+            Debug.LogError("Current objective number is out of range");
     }
 }
