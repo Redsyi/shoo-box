43f6b7a076716df97333e46f0357f9c0ac8e0c5a
unknown <nathan.stubbs@utexas.edu>
2020-04-21 23:37:33 -0500

    better plants

---
+++ Assets/Scripts/Objects/PlayEffectOnKick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PlayEffectOnKick : MonoBehaviour, IKickable
+{
+    public VisualEffect effect;
+
+    void Start()
+    {
+        if (!effect)
+            effect = GetComponent<VisualEffect>();
+    }
+
+    public void OnKick(GameObject kicker)
+    {
+        effect.Play();
+    }
+}
