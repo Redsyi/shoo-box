79aacce1aedccb4fa04a3c5021f4ce7b899a9db8
Jake Crabtree <uxsee3@gmail.com>
2020-04-28 21:15:10 -0700

    button light up tutorial, more shadows

---
+++ Assets/Scripts/Objects/SwitchMaterialOnKick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMaterialOnKick : MonoBehaviour, IKickable
+{
+    public Renderer renderer;
+    public Material targetMaterial;
+
+    public void OnKick(GameObject kicker)
+    {
+        renderer.material = targetMaterial;
+    }
+
+    private void Start()
+    {
+        if (renderer == null)
+            Debug.LogError("render not attached to materialkick");
+    }
+}
