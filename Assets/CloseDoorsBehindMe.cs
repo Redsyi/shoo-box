2dec1cd7bb3c2ee9950ae25388aba92b6e35cf2f
Jake Crabtree <uxsee3@gmail.com>
2020-04-29 10:50:40 -0700

    doors

---
+++ Assets/CloseDoorsBehindMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoorsBehindMe : MonoBehaviour
+{
+
+    public Animator animator;
+    public string trigger;
+    public string startTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetTrigger(startTrigger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger(trigger);
+        Destroy(gameObject);
+    }
+}
