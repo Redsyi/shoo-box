b34a1749a5c7557eb3ba0d82b2fb836bde43e6a3
Jake Crabtree <uxsee3@gmail.com>
2020-05-04 20:06:42 -0700

    start to mouse controls

---
+++ Assets/Scenes/testing and temp scenes/ClickPlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickPlayerMovement : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    [SerializeField] private LayerMask movableLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo, 100, movableLayer))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
