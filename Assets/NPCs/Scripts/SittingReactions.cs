efa187d65c1be5467c058b65134bc035aea0ee9f
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-05-13 16:53:45 -0500

    Fixed bugs in lobby, changed mouse sensitivity, and took out debug prints

--- Assets/NPCs/Scripts/SittingReactions.cs
+++ Assets/NPCs/Scripts/SittingReactions.cs
@@ -10,13 +10,11 @@ public class SittingReactions : MonoBehaviour, IKickable, ISandalable
     public Animator animator;
     public void HitBySandal()
     {
-        print("Hit by sandal");
         animator.SetTrigger("hitBySandal");
     }
 
     public void OnKick(GameObject kicker)
     {
-        print("Kicked");
         animator.SetTrigger("hitByBoots");
     }
 }
