014a3145db422b2ff15bb0742a53d226f6ff386b
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-06-10 00:55:51 -0500

    Checkpoints will no longer set if player is being chased by enemies

--- Assets/Checkpoints/Scripts/SetCheckpointOnEnter.cs
+++ Assets/Checkpoints/Scripts/SetCheckpointOnEnter.cs
@@ -10,7 +10,6 @@ public class SetCheckpointOnEnter : MonoBehaviour
 {
     CheckpointManager checkpointManager;
     public int checkpointID;
-
     void Start()
     {
         checkpointManager = FindObjectOfType<CheckpointManager>();
@@ -18,15 +17,19 @@ public class SetCheckpointOnEnter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Grab the checkpoint message object. Should be the only object where tag == Checkpoint Message
-        Animator message = GameObject.FindGameObjectWithTag("Checkpoint Message").GetComponent<Animator>();
-        if (message)
+        Player player = FindObjectOfType<Player>();
+        if (player && player.npcsChasing < 1) // Only set checkpoint if player isn't being chased
         {
-            message.SetTrigger("Checkpoint Hit");
+            // Grab the checkpoint message object. Should be the only object where tag == Checkpoint Message
+            Animator message = GameObject.FindGameObjectWithTag("Checkpoint Message").GetComponent<Animator>();
+            if (message)
+            {
+                message.SetTrigger("Checkpoint Hit");
+            }
+            else
+                Debug.LogError("Checkpoint message object not found");
+            checkpointManager.SetCheckpoint(checkpointID);
+            gameObject.SetActive(false);
         }
-        else
-            Debug.LogError("Checkpoint message object not found");
-        checkpointManager.SetCheckpoint(checkpointID);
-        gameObject.SetActive(false);
     }
 }
